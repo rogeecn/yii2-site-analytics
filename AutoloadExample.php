498ccd3c4f89fbb4882341d83de68544de613320
杨豪 <yanghao@chukong-inc.com>
2015-06-10 17:27:26 +0800

    init

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rogee;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
